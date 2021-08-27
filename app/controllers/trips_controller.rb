5f92bc2c8ec6376f5ff4fe0b6ce3b42e5cc18167
Martial Salgues <github@mar7ius.me>
2021-08-27 14:17:02 +0200

    Map page done

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -40,9 +40,9 @@ class TripsController < ApplicationController
     TripFlight.destroy_all
     Flight.destroy_all
     amadeus = Amadeus::Client.new({
-      client_id: 'eswpB1iV5JFtkn4KWssBCAQsc4jdSQsh',
-      client_secret: 'Q1PG0GhWGtDcmN4N'
-     })
+      client_id: "eswpB1iV5JFtkn4KWssBCAQsc4jdSQsh",
+      client_secret: "Q1PG0GhWGtDcmN4N",
+    })
     result_d = amadeus.shopping.flight_offers_search.get(originLocationCode: params[:departure], destinationLocationCode: params[:arrival], departureDate: params[:departure_date], adults: 1, nonStop: true)
 
     parsing = JSON.parse(result_d.body)["data"].first(3)
@@ -131,40 +131,57 @@ class TripsController < ApplicationController
     # set origin point - actualy, san francisco
     san_francisco = Activity.where(name: "San Francisco")
     # that bitch return an array, need the index 0:
-    request = "#{san_francisco[0].latitude}%2C#{san_francisco[0].longitude}%3A"
+    waypoints = []
 
+    origin_point = "#{san_francisco[0].latitude}%2C#{san_francisco[0].longitude}%3A"
     # Build string of Requested coordinates :
     @trip.activities.each do |waypoint|
-      request += "#{waypoint.latitude}%2C#{waypoint.longitude}%3A"
+      # request += "#{waypoint.latitude}%2C#{waypoint.longitude}%3A"
+      waypoints << "#{waypoint.latitude}%2C#{waypoint.longitude}%3A"
     end
-    request += "#{san_francisco[0].latitude}%2C#{san_francisco[0].longitude}%3A"
+
+
+    trip_request = origin_point + waypoints.join + origin_point
 
     # API Call
     api_token = "UIJntIl4JdzoPutRU5kcksjwlzPSDGlR"
-    tomtom_request = "https://api.tomtom.com/routing/1/calculateRoute/#{request}/json?computeBestOrder=true&routeRepresentation=polyline&routeType=fastest&avoid=unpavedRoads&travelMode=car&vehicleCommercial=false&key=#{api_token}"
+    tomtom_request = "https://api.tomtom.com/routing/1/calculateRoute/#{trip_request}/json?computeBestOrder=true&routeRepresentation=polyline&routeType=fastest&avoid=unpavedRoads&travelMode=car&vehicleCommercial=false&key=#{api_token}"
     response_serialized = URI.open(tomtom_request).read
     response = JSON.parse(response_serialized, object_class: OpenStruct)
 
-
     # Exploiting results :
     full_trip_road = [] # Contains all routes to pass to mapbox
-    @time_for_each_leg = [] # Contains time to drive betwenn each activities
-    @step_order = [] # Contains activites record in the right order
+    # @time_for_each_leg = [] # Contains time to drive betwenn each activities
+    # @step_order = [] # Contains activites record in the right order
 
     response.routes.first.legs.each do |leg|
-      @time_for_each_leg << leg.summary.travelTimeInSeconds
-      @step_order << [leg.points.first, leg.points.last]
+      # @time_for_each_leg << leg.summary.travelTimeInSeconds
+      # @step_order << [leg.points.first, leg.points.last]
       leg.points.each do |coordinates|
         full_trip_road << [coordinates.longitude, coordinates.latitude]
       end
     end
 
-    # Extract Order an activity name with returned optimized order
-    # @step_order contains OpenStruct data & it's coordinates
-    @step_order.map! do |step|
-      # step = Activity.where(latitude: step[0].latitude.truncate(3))
-      step = Activity.where("latitude > ? AND latitude < ?", step[0].latitude - 0.0001, step[0].latitude + 0.0001)
-    end.flatten!
+    # Fabrication d'un hash de data compilé complet :
+    @trip_summary = {}
+    @trip_summary[:originPointActivityId] = san_francisco[0].id
+
+    # /!\ Optimized = array de coord passé dans la requete // Provided = Ordre intineraire optimisé
+    response.optimizedWaypoints.each do |waypoint|
+      @trip_summary["waypoint#{waypoint.providedIndex}ActivityId".to_sym] = Activity.find(@trip.activities[waypoint.optimizedIndex].id).id
+    end
+    @trip_summary[:finalPointActivityId] = san_francisco[0].id
+
+    # Ajout des temps pour chaque etapes :
+    @trip_summary[:travelTimeBetweenWaypoint] = [{
+      originToWaypoint1: response.routes.first.legs.first.summary.travelTimeInSeconds
+    }]
+
+    (1...(response.routes.first.legs.count - 1)).to_a.each do |step|
+      @trip_summary[:travelTimeBetweenWaypoint].first["waypoint#{step}To#{step + 1}".to_sym] = response.routes.first.legs[step].summary.travelTimeInSeconds
+    end
+
+    @trip_summary[:travelTimeBetweenWaypoint].first[:lastWaypointToFinalPoint] = response.routes.first.legs.last.summary.travelTimeInSeconds
 
 
     # Send dataset for markers
@@ -172,7 +189,7 @@ class TripsController < ApplicationController
       {
         lat: waypoint[:latitude],
         lng: waypoint[:longitude],
-        info_window: render_to_string(partial: "shared/info_window", locals: { waypoint: waypoint })
+        info_window: render_to_string(partial: "shared/info_window", locals: { waypoint: waypoint }),
       }
     end
     # @routes = routes.flatten(1)
--- db/seeds.rb
+++ db/seeds.rb
@@ -62,7 +62,7 @@ ACTIVITIES = [{
 
   {
   name: "Los Angeles",
-  address: "Los Angeles California, United-Statess",
+  address: "Los Angeles California, United-States",
   longitude: -118.243685,
   latitude: 34.052234,
   category: 'city',
@@ -75,8 +75,8 @@ ACTIVITIES = [{
   {
   name: "Disneyland",
   address: "1313 Disneyland Dr, Anaheim, CA 92802, United-States",
-  longitude: -117.915542,
-  latitude: 33.809742,
+  longitude: -117.923709,
+  latitude: 33.815412,
   category: 'amusement park',
   description: "Explore the magic of Disneyland Park and Disney California Adventure Park.",
   duration: 360,
@@ -98,9 +98,9 @@ ACTIVITIES = [{
 
     {
   name: "Universal Studios Hollywood",
-  address: " 100 Universal City Plaza, Universal City, CA 91608, United-States",
-  longitude: -118.352898,
-  latitude: 34.141354,
+  address: "100 Universal City Plaza, Universal City, CA 91608, United-States",
+  longitude: -118.351175,
+  latitude: 34.135315,
   category: 'amusement park',
   description: "Universal Studios Hollywood is a theme park owned by NBC Universal and located in Universal City in the suburbs of Los Angeles, California. The park is located next to the Universal Studios owned by Universal Pictures, which is part of the tour with the Studio Tour.",
   duration: 420,
@@ -110,9 +110,9 @@ ACTIVITIES = [{
 
   {
   name: "Private tour of the Griffith Observatory ",
-  address: " 2800 E Observatory Rd, Los Angeles, CA 90027, United-States",
-  longitude: -118.294197,
-  latitude: 34.136555,
+  address: "2800 E Observatory Rd, Los Angeles, CA 90027, United-States",
+  longitude: -118.300463,
+  latitude: 34.119311,
   category: 'visit',
   description: " Called the hood ornament of Los Angeles, it serves as a public observatory, planetarium and exhibit space. Designed by some of its former employees, this tour will give you an introduction to the city you'll never forget. ",
   duration: 120,
@@ -122,7 +122,7 @@ ACTIVITIES = [{
 
   {
   name: "Hollywood Walk of Fame",
-  address: " Hollywood Boulevard, Vine St, Los Angeles, CA 90028, United-States",
+  address: "Hollywood Boulevard, Vine St, Los Angeles, CA 90028, United-States",
   longitude: -118.333694,
   latitude: 34.101639,
   category: 'walk',
@@ -134,9 +134,9 @@ ACTIVITIES = [{
 
     {
   name: "Hollywood Sign",
-  address: " Los Angeles, Californie 90068, United-States",
-  longitude: -118.321548,
-  latitude: 34.134115,
+  address: "Los Angeles, Californie 90068, United-States",
+  longitude: -118.320674,
+  latitude: 34.134532,
   category: 'walk',
   description: "The Hollywood Sign is more than just nine white letters spelling out a city's name. It’s one of the world's most famous monuments and a universal metaphor for ambition, success, glamour.",
   duration: 60,
@@ -147,8 +147,8 @@ ACTIVITIES = [{
       {
   name: "San Diego Zoo Safari Park",
   address: " 15500 San Pasqual Valley Rd, Escondido, CA 92027, United-States",
-  longitude: -117.149048,
-  latitude: 32.735317,
+  longitude: -117.002091,
+  latitude: 33.095548,
   category: 'zoological park',
   description: "San Diego Zoo Safari Park is a zoological park in San Pasqual Valley in San Diego, California, near Escondido. The park is home to a wide range of wild and endangered animals and species from every continent.",
   duration: 360,
@@ -158,7 +158,7 @@ ACTIVITIES = [{
 
    {
   name: "Death Valley National Park",
-  address: " Furnace Creek, CA 92328, United-States",
+  address: "Furnace Creek, CA 92328, United-States",
   longitude: -116.808972,
   latitude: 36.421045,
   category: 'national park',
@@ -169,10 +169,10 @@ ACTIVITIES = [{
   },
 
   {
-  name: "Red Rock Canyon ",
+  name: "Red Rock Canyon",
   address: "1000 Scenic Loop Dr, Las Vegas, NV 89161, United-States",
-  longitude: -115.428135,
-  latitude: 36.135626,
+  longitude: -115.423827,
+  latitude: 36.132108,
   category: 'national park',
   description: "Red Rock Canyon is located 20 km west of Las Vegas in the state of Nevada in the southwestern United States. It belongs to the arid environment of the Mojave Desert. ",
   duration: 180,
@@ -193,10 +193,10 @@ ACTIVITIES = [{
   },
 
   {
-  name: "Las Vegas Cirdue du soleil shows -Bellagio Hotel",
-  address: " 95 Av. du Président Allende, 94800 Villejuif",
-  longitude: -115.176704,
-  latitude: 36.112625,
+  name: "Las Vegas Cirque du soleil shows - Bellagio Hotel",
+  address: "Bellagio Drive, Las Vegas, NV 89109, United-States",
+  longitude: -115.1743,
+  latitude: 36.11157,
   category: 'show',
   description: "Immerse yourself in the world of O, Cirque du Soleil's original water show at the Bellagio that ranks among the top activities in Las Vegas.",
   duration: 90,
@@ -219,8 +219,8 @@ ACTIVITIES = [{
   {
   name: "San Francisco",
   address: " San Francisco California, United-States",
-  longitude: -122.3929,
-  latitude: 37.6216,
+  longitude: -122.431297,
+  latitude: 37.773972,
   category: 'city',
   description: "San Francisco is a large city located on the west coast of the United States of America, in the state of California.",
   duration: 900,
@@ -231,8 +231,8 @@ ACTIVITIES = [{
   {
   name: "Golden Gate Bridge",
   address: "Golden Gate Bridge, San Francisco, CA, United-States ",
-  longitude: -122.476944,
-  latitude: 37.769722,
+  longitude: -122.47872,
+  latitude: 37.82034,
   category: 'walk',
   description: "The Golden Gate Bridge is a suspension bridge spanning the Golden Gate, the one-mile-wide (1.6 km) strait connecting San Francisco Bay and the Pacific Ocean.",
   duration: 90,
@@ -243,8 +243,8 @@ ACTIVITIES = [{
     {
   name: "Alcatraz Federal Penitentiary",
   address: "San Francisco, Californie 94133, United-States",
-  longitude: -122.422844,
-  latitude: 37.828125,
+  longitude: -122.41074,
+  latitude: 37.81749,
   category: 'museum',
   description: "The Rock was a maximum security federal prison on Alcatraz Island, 1.25 miles (2.01 km) off the coast of San Francisco,",
   duration: 180,
@@ -267,8 +267,8 @@ ACTIVITIES = [{
     {
   name: "Sequoia National Park",
   address: "California, United-States",
-  longitude: -118.565750,
-  latitude: 36.486366,
+  longitude: -118.59584,
+  latitude: 36.45288,
   category: 'national park',
   description: "Sequoia National Park is located in the southern Sierra Nevada Mountains of California. It is known for its huge redwoods, including General Sherman, which dominates the Giant Forest.",
   duration: 400,
@@ -276,7 +276,7 @@ ACTIVITIES = [{
   code_image: "020"
   },
      {
-  name: "santa monica pier",
+  name: "Santa Monica Pier",
   address: "200 Santa Monica Pier, Santa Monica, CA 90401, United-States",
   longitude: -118.497269,
   latitude: 34.008991,
@@ -289,8 +289,8 @@ ACTIVITIES = [{
       {
   name: "SeaWorld - San Diego",
   address: "500 Sea World Dr., San Diego, CA 92109, United-States",
-  longitude: -117.226608,
-  latitude: 32.764790,
+  longitude: -117.230969,
+  latitude: 32.763173,
   category: 'amusemet park',
   description: "SeaWorld San Diego is home to world-class animal shows, presentations and exhibits, spread out on beautiful Mission Bay Park.",
   duration: 240,
@@ -322,8 +322,8 @@ ACTIVITIES = [{
      {
   name: "Mammoth Mountain",
   address: "10001 Minaret Road Mammoth Lakes, CA 93546, United-States",
-  longitude: -119.032631,
-  latitude: 37.630768,
+  longitude: -119.039352,
+  latitude: 37.650301,
   category: 'walk',
   description: "Mammoth Mountain is a volcano that lies west of the town of Mammoth Lakes in the Inyo National Forest. Mammoth Mountain is famous for its ski area and its strong snow cover due to its privileged location in the Sierra Nevada..",
   duration: 120,
@@ -342,10 +342,10 @@ ACTIVITIES = [{
   code_image: "026"
   },
      {
-  name: "Newport Landing Whale Watching”",
+  name: "Newport Landing Whale Watching",
   address: "309 Palm St A, Newport Beach, CA 92661, United-States ",
-  longitude: -117.927933,
-  latitude: 33.628342,
+  longitude: -117.900461,
+  latitude: 33.603225,
   category: 'show',
   description: "Newport Landing is ideally positioned to take advantage of the annual Grey whale migration, which brings hundreds of Grey Whales along the Laguna Beach.",
   duration: 120,
@@ -388,12 +388,13 @@ ACTIVITIES = [{
   },
  ]
 
+ puts "creating activities :"
 ACTIVITIES.each do |activity|
   Activity.create!(activity)
  end
 
 
-
+# puts "creating hotels :"
 Hotel.create(
   name: "St. Regis San Francisco",
   address: "125 Third Street, San Francisco, CA 94103, United-States",
