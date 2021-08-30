4b756321bf83bddb4bbfa388c9df5e479ff084e7
Martial Salgues <github@mar7ius.me>
2021-08-30 10:32:17 +0200

    changed returningg date param in flight call to the nb of days enter

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -4,6 +4,7 @@ require "amadeus"
 
 class TripsController < ApplicationController
   skip_before_action :authenticate_user!, only: :new
+  before_action :set_trip, only: [:step_one]
 
   def index
     @trips = policy_scope(Trip)
@@ -27,10 +28,10 @@ class TripsController < ApplicationController
     @trip.user = current_user
     departure = params[:trip][:departure_flight]
     arrival = params[:trip][:arrival_flight]
-    departure_date = params[:trip][:start_date]
-    # raise
+
+
     if @trip.save!
-      redirect_to step_one_trip_path(@trip, departure: departure, arrival: arrival, departure_date: departure_date)
+      redirect_to step_one_trip_path(@trip, departure: departure, arrival: arrival)
     else
       render :new
     end
@@ -43,7 +44,7 @@ class TripsController < ApplicationController
       client_id: "nXHIAKb8yz6m3otvA1MGO2ETNK3I0gtm",
       client_secret: "tTW6I4texIOPcBLr",
     })
-    result_d = amadeus.shopping.flight_offers_search.get(originLocationCode: params[:departure], destinationLocationCode: params[:arrival], departureDate: params[:departure_date], adults: 1, nonStop: true)
+    result_d = amadeus.shopping.flight_offers_search.get(originLocationCode: params[:departure], destinationLocationCode: params[:arrival], departureDate: @trip.start_date, adults: 1, nonStop: true)
 
     parsing = JSON.parse(result_d.body)["data"].first(3)
 
@@ -60,7 +61,7 @@ class TripsController < ApplicationController
         airport_iata_code: flight["itineraries"][0]["segments"][0]["departure"]["iataCode"],
       )
     end
-    result_a = amadeus.shopping.flight_offers_search.get(originLocationCode: params[:arrival], destinationLocationCode: params[:departure], departureDate: "2021-11-02", adults: 1, nonStop: true)
+    result_a = amadeus.shopping.flight_offers_search.get(originLocationCode: params[:arrival], destinationLocationCode: params[:departure], departureDate: @trip.start_date + @trip.duration.days, adults: 1, nonStop: true)
     parsing = JSON.parse(result_a.body)["data"].first(3)
     parsing.each do |flight|
       Flight.create(
@@ -76,7 +77,6 @@ class TripsController < ApplicationController
       )
     end
 
-    set_trip
     @flight_departure = TripFlight.new
     @flight_departure_list = Flight.where(departure_flight: true)
     @flight_returning = TripFlight.new
@@ -136,11 +136,9 @@ class TripsController < ApplicationController
 
     origin_point = "#{san_francisco[0].latitude}%2C#{san_francisco[0].longitude}%3A"
     # Build string of Requested coordinates :
-    @trip.activities.each do |waypoint|
-      # request += "#{waypoint.latitude}%2C#{waypoint.longitude}%3A"
-      waypoints << "#{waypoint.latitude}%2C#{waypoint.longitude}%3A"
+    @trip.activities.each do |activity|
+      waypoints << "#{activity.latitude}%2C#{activity.longitude}%3A"
     end
-
 
     trip_request = origin_point + waypoints.join + origin_point
 
@@ -150,32 +148,37 @@ class TripsController < ApplicationController
     response_serialized = URI.open(tomtom_request).read
     response = JSON.parse(response_serialized, object_class: OpenStruct)
 
+    # ----------------------
+    # add response error handeling by response.respond_to?(:routes) / else : render activity 'no routes find'
+    # -----------------------
+
     # Exploiting results :
-    full_trip_road = [] # Contains all routes to pass to mapbox
-    # @time_for_each_leg = [] # Contains time to drive betwenn each activities
-    # @step_order = [] # Contains activites record in the right order
+    @routes = [] # Contains all routes to pass to mapbox
 
     response.routes.first.legs.each do |leg|
-      # @time_for_each_leg << leg.summary.travelTimeInSeconds
-      # @step_order << [leg.points.first, leg.points.last]
       leg.points.each do |coordinates|
-        full_trip_road << [coordinates.longitude, coordinates.latitude]
+        @routes << [coordinates.longitude, coordinates.latitude]
       end
     end
 
     # Fabrication d'un hash de data compilé complet :
     @trip_summary = {}
+    @trip_summary[:totalWaypoints] = 0
     @trip_summary[:originPointActivityId] = san_francisco[0].id
 
     # /!\ Optimized = array de coord passé dans la requete // Provided = Ordre intineraire optimisé
     response.optimizedWaypoints.each do |waypoint|
-      @trip_summary["waypoint#{waypoint.providedIndex}ActivityId".to_sym] = Activity.find(@trip.activities[waypoint.optimizedIndex].id).id
+      activity = Activity.find(@trip.activities[waypoint.optimizedIndex].id)
+      # @trip_summary["waypoint#{waypoint.providedIndex}ActivityId".to_sym] = Activity.find(@trip.activities[waypoint.optimizedIndex].id).id
+      @trip_summary[:totalWaypoints] += 1
+      @trip_summary["waypoint#{waypoint.providedIndex + 1}ActivityId".to_sym] = activity.id
+      @trip_summary["waypoint#{waypoint.providedIndex + 1}ActivityDuration".to_sym] = activity.duration * 60
     end
     @trip_summary[:finalPointActivityId] = san_francisco[0].id
 
     # Ajout des temps pour chaque etapes :
     @trip_summary[:travelTimeBetweenWaypoint] = [{
-      originToWaypoint1: response.routes.first.legs.first.summary.travelTimeInSeconds
+      originToWaypoint1: response.routes.first.legs.first.summary.travelTimeInSeconds,
     }]
 
     (1...(response.routes.first.legs.count - 1)).to_a.each do |step|
@@ -183,7 +186,6 @@ class TripsController < ApplicationController
     end
 
     @trip_summary[:travelTimeBetweenWaypoint].first[:lastWaypointToFinalPoint] = response.routes.first.legs.last.summary.travelTimeInSeconds
-
 
     # Send dataset for markers
     @markers = @trip.activities.map do |waypoint|
@@ -193,8 +195,83 @@ class TripsController < ApplicationController
         info_window: render_to_string(partial: "shared/info_window", locals: { waypoint: waypoint }),
       }
     end
-    # @routes = routes.flatten(1)
-    @routes = full_trip_road
+
+    @durations = []
+
+    @durations << @trip_summary[:travelTimeBetweenWaypoint].first[:originToWaypoint1]
+    (1..@trip_summary[:totalWaypoints]).to_a.each do |numb|
+      @durations << @trip_summary["waypoint##{numb}ActivityDuration".to_sym]
+      if @trip_summary[:travelTimeBetweenWaypoint].first.key?("waypoint#{numb}To#{numb + 1}".to_sym)
+        @durations << @trip_summary[:travelTimeBetweenWaypoint].first["waypoint#{numb}To#{numb + 1}".to_sym]
+      end
+    end
+    @durations << @trip_summary[:travelTimeBetweenWaypoint].first[:lastWaypointToFinalPoint]
+
+    @all_days = []
+    day_constant = 36.000 # Seconds
+
+    while @durations.any?
+      remaining_time = day_constant
+      trip_day = []
+      while (remaining_time - @durations.first).positive?
+        trip_day.push(@durations.first)
+        remaining_time -= @durations.first
+        @durations.shift
+      end
+      if @durations.first > day_constant
+        @durations.first -= day_constant
+        trip_day.push(day_constant)
+      elsif remaining_time > 120 # && @duration.first = trajet
+        trip_day.push(remaining_time)
+        @durations.first -= remaining_time
+      end
+      @all_days.push(trip_day)
+    end
+    # repartition par jours :
+
+    #   if originToFirstWaypoint < day_time
+    #     day += originToFirstWaypoint
+    #     until day + transitToNextPoint > day_time
+    #           day += transitToNextPoint
+    #           waypoint += 1
+    #     end
+    #   (2...@trip_summary[:travelTimeBetweenWaypoint].first.count)
+    #   if temps de trajet + activityWaypoint1duration < day_time
+    #       true : day = temps de trajet + activityWaypoint1duration
+    #       until day + transitToNextPoint > day_time
+    #           day += transitToNextPoint
+    #           waypoint += 1
+    #       end
+    #   else : day = temps de trajet & findHotel sur trajet originToWaypoint à moins de 2h de waypointNext + set day info
+    #
+    #
+    #
+    #
+    #         if temps de trajet + activityWaypoint1duration + trajetWaypoint1ToNext < day_time
+    #           if temps de trajet + activityWaypoint1duration + trajetWaypoint1ToNext + activityNewtWaypointDuration < day_time
+    #   else
+    #    findHotel sur trajet originToWaypoint à moins de 2h de waypoint1
+    #   end
+    #
+    #
+    #
+    # trajets = [1500,250,18078,280,12,244,421]
+    # activity = [900,480,850,240,90,20]
+    #
+    # day = 28800
+    # day_num = n
+    # remaining_day_time
+    # while remaining_day_time < day
+    #   if remaining_day_time + trajet[n] < day
+    #     remaining_day_time += trajet[n]
+    #     trajet.upshift(n)
+    #   end
+    #   if remaining_day_time + activity[n] < day
+    #     remaining_day_time += activity[n]
+    #     activity.upshift[n]
+    #   end
+    #
+    raise
   end
 
   def step_three
