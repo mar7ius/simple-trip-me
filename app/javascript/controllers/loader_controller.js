3023357197f06df959cb9c986b40a647c1ea2adc
Martial Salgues <github@mar7ius.me>
2021-09-01 13:26:42 +0200

    implement loader while compiling trip

---
+++ app/javascript/controllers/loader_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = ["modal", "submit"]
+
+  connect() {
+    // this.outputTarget.textContent = 'Hello, Stimulus!'
+    console.log(this.modalTarget)
+    console.log(this.submitTarget)
+  }
+
+  submit() {
+    this.modalTarget.classList.remove('d-none');
+  }
+}
