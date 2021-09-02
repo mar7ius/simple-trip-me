76a38811c515884dee02e827fa070e201873cf88
diane orlach <diane@lesateliersdelamarmotte.com>
2021-09-02 10:57:22 +0200

    fix duration h min

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,4 +3,13 @@ class Activity < ApplicationRecord
 
   accepts_nested_attributes_for :trip_activities
   # validates :address, :category, :description, :price, :day, :duration, presence: true
+  def formatted_duration
+    hours = self.duration / 60
+    minutes = self.duration % 60
+    if minutes == 0
+      "#{ hours }h"
+    else
+      "#{ hours }h#{minutes }min"
+    end
+  end
 end
